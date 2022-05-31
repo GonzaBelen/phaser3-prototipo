1649138575a6e9c4fc913a77380828b65f4687b4
SebaOlliari <seba.olliari@gmail.com>
2022-05-31 20:18:56 -0300

    segundo commit

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -107,7 +107,7 @@ update ()
     player.anims.play('turn');
   }
 
-  if (cursors.up.isDown && player.body.touching.down)
+  if (cursors.up.isDown && player.body.blocked.down)
   {
     player.setVelocityY(-375);
   }
@@ -120,10 +120,10 @@ collectRedStar (player, redStar)
   scoreText.setText('Puntuación: ' + score);    
   if ((redStars.countActive(true) === 0) && (stars.countActive(true) === 0)) {
     stars.children.iterate(function (child) {
-      child.enableBody(true, child.x, child.y + 10, true, true);
+      child.enableBody(true, child.x, child.y, true, true);
     }),
     redStars.children.iterate(function (child) {
-      child.enableBody(true, child.x, child.y + 10, true, true);
+      child.enableBody(true, child.x, child.y, true, true);
     });
 
     var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
@@ -141,13 +141,12 @@ collectStar (player, star)
   star.disableBody(true, true);
   score += 10;
   scoreText.setText('Puntuación: ' + score);
-  if ((redStars.countActive(true) === 0) && (stars.countActive(true) === 0))
-  {
+  if ((redStars.countActive(true) === 0) && (stars.countActive(true) === 0)) {
     stars.children.iterate(function (child) {
-      child.enableBody(true, child.x, child.y + 10, true, true);
+      child.enableBody(true, child.x, child.y, true, true);
     }),
     redStars.children.iterate(function (child) {
-      child.enableBody(true, child.x, child.y + 10, true, true);
+      child.enableBody(true, child.x, child.y, true, true);
     });
 
     var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
@@ -177,164 +176,3 @@ hitBomb (player, bomb) {
   }, 1000);
 }
 }
-
-
-
-
-
-
-
-
-
-
-  /*create()
-  {
-    this.add.image(400, 300, 'sky');
-  
-    platforms = this.physics.add.staticGroup();
-  
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-  
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(0, 75, 'ground');
-    platforms.create(750, 220, 'ground');
-  
-    player = this.physics.add.sprite(100, 450, 'dude');
-  
-    player.setBounce(0.1);
-    player.setCollideWorldBounds(true);
-  
-    cursors = this.input.keyboard.createCursorKeys();
-        
-    stars = this.physics.add.group({
-      key: 'star',
-      repeat: 6,
-      setXY: { x: 12, y: 0, stepX: 130 }
-    });
-  
-    redStars = this.physics.add.group({
-      key: 'redStar',
-      repeat: 5,
-      setXY: { x: 75, y: 0, stepX: 130 }
-    });
-  
-    stars.children.iterate(function (child) {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));  
-    });
-  
-    redStars.children.iterate(function (child) {  
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)); 
-    });
-  
-    bombs = this.physics.add.group();
-    
-    scoreText = this.add.text(16, 16, 'Puntuación: 0', { fontSize: '32px', fill: '#800080', fontFamily: "Arial"});
-
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(stars, platforms);
-    this.physics.add.collider(redStars, platforms);
-    this.physics.add.collider(bombs, platforms);
-  
-    this.physics.add.overlap(player, stars, this.collectStar, null, this);
-    this.physics.add.overlap(player, redStars, this.collectRedStar, null, this);
-  
-    this.physics.add.collider(player, bombs, this.hitBomb, null, this);
-
-    gameOver = false;
-    score = 0;
-  }
-
-  update ()
-  {
-    if (gameOver){
-      return;
-    }
-
-    if (cursors.left.isDown) {
-      player.setVelocityX(-160);
-      player.anims.play('left', true);
-    }
-    else if (cursors.right.isDown)
-    {
-      player.setVelocityX(160);
-      player.anims.play('right', true);
-    }
-    else
-    {
-      player.setVelocityX(0);
-      player.anims.play('turn');
-    }
-
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-      player.setVelocityY(-375);
-    }
-  }
-
-  collectRedStar (player, redStar)
-  {
-    redStar.disableBody(true, true);
-    score += 15;
-    scoreText.setText('Puntuación: ' + score);    
-    if ((redStars.countActive(true) === 0) && (stars.countActive(true) === 0))
-    {
-      stars.children.iterate(function (child) {
-        child.enableBody(true, child.x, 0, true, true);
-        }),
-        redStars.children.iterate(function (child) {
-          child.enableBody(true, child.x, 0, true, true);
-        });
-
-      var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-      var bomb = bombs.create(x, 16, 'bomb');
-      bomb.setBounce(1);
-      bomb.setCollideWorldBounds(true);
-      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-      bomb.allowGravity = false;
-    }
-  };
-
-  collectStar (player, star)
-  {
-    star.disableBody(true, true);
-    score += 10;
-    scoreText.setText('Puntuación: ' + score);
-    if ((redStars.countActive(true) === 0) && (stars.countActive(true) === 0))
-    {
-      stars.children.iterate(function (child) {
-        child.enableBody(true, child.x, 0, true, true);
-      }),
-      redStars.children.iterate(function (child) {
-        child.enableBody(true, child.x, 0, true, true);
-      });
-
-      var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-      var bomb = bombs.create(x, 16, 'bomb');
-      bomb.setBounce(1);
-      bomb.setCollideWorldBounds(true);
-      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-      bomb.allowGravity = false;
-    }
-  }
-
-  hitBomb (player, bomb) {
-    this.physics.pause();
-
-    player.setTint(0xff0000);
-
-    player.anims.play('turn');
-
-    gameOver = true;
-
-    setTimeout(() => {
-      this.scene.start(
-        "Retry",
-        { score: score } 
-      );
-    }, 1000);
-  }
-}
-*/
